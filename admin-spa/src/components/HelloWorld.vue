c300ad3f97ac3cedba730ba6fa9ba828293b5885
Ben Jeckson <thanhtikexaw@gmail.com>
2023-03-21 02:38:49 +0630

    adding vue+vite+tailwind for admin spa

---
+++ admin-spa/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div class="bg-purple-500">
+        Hello world
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
