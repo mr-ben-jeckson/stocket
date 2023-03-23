ae82ed5bc9dec100fa240a4ac41899c6f7be7cc0
Ben Jeckson <thanhtikexaw@gmail.com>
2023-03-23 13:39:20 +0630

    fix

--- admin-spa/src/components/GuestsLayout.vue
+++ admin-spa/src/components/GuestsLayout.vue
@@ -1,5 +1,12 @@
 <template>
-
+    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="w-full max-w-md space-y-8">
+            <div>
+                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">{{ title }}</h2>
+            </div>
+        </div>
+        <slot></slot>
+    </div>
 </template>
 
 <script>
@@ -7,6 +14,4 @@ export default {
     name: "GuestsLayout"
 }
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
--- admin-spa/src/views/login.vue
+++ admin-spa/src/views/login.vue
@@ -1,12 +1,4 @@
 <template>
-    <!--
-      This example requires updating your template:
-
-      ```
-      <html class="h-full bg-gray-50">
-      <body class="h-full">
-      ```
-    -->
     <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div class="w-full max-w-md space-y-8">
             <div>
