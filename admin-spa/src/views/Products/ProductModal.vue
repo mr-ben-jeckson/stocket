a512d3691c3af59277614a2f7090577339d37ee0
Ben Jeckson <thanhtikexaw@gmail.com>
2023-04-11 22:17:03 +0630

    product modal update

--- admin-spa/src/views/Products/ProductModal.vue
+++ admin-spa/src/views/Products/ProductModal.vue
@@ -1,90 +1,99 @@
 <template>
-    <TransitionRoot appear :show="show" as="template">
-      <Dialog as="div" @close="closeModal" class="relative z-10">
-        <TransitionChild
-          as="template"
-          enter="duration-300 ease-out"
-          enter-from="opacity-0"
-          enter-to="opacity-100"
-          leave="duration-200 ease-in"
-          leave-from="opacity-100"
-          leave-to="opacity-0"
-        >
-          <div class="fixed inset-0 bg-black bg-opacity-25" />
-        </TransitionChild>
+  <TransitionRoot appear :show="show" as="template">
+    <Dialog as="div" @close="closeModal" class="relative z-10">
+      <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
+        leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
+        <div class="fixed inset-0 bg-black bg-opacity-25" />
+      </TransitionChild>
 
-        <div class="fixed inset-0 overflow-y-auto">
-          <div
-            class="flex min-h-full items-center justify-center p-4 text-center"
-          >
-            <TransitionChild
-              as="template"
-              enter="duration-300 ease-out"
-              enter-from="opacity-0 scale-95"
-              enter-to="opacity-100 scale-100"
-              leave="duration-200 ease-in"
-              leave-from="opacity-100 scale-100"
-              leave-to="opacity-0 scale-95"
-            >
-              <DialogPanel
-                class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
-              >
-                <DialogTitle
-                  as="h3"
-                  class="text-lg font-medium leading-6 text-gray-900"
-                >
-                  Payment successful
+      <div class="fixed inset-0 overflow-y-auto">
+        <div class="flex min-h-full items-center justify-center p-4 text-center">
+          <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95">
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <loader v-if="loading"
+                class="absolute left-0 top-0 bg-white right-0 bottom-0 flex items-center justify-center" />
+              <header class="py-3 px-4 flex justify-between items-center">
+                <DialogTitle as="h3" class="text-lg leading-6 font-medium text-gray-900">
+                  {{ product.id ? `Update Product: "${props.product.title}"` : 'Create new Product' }}
                 </DialogTitle>
-                <div class="mt-2">
-                  <p class="text-sm text-gray-500">
-                    Your payment has been successfully submitted. We’ve sent you
-                    an email with all of the details of your order.
-                  </p>
-                </div>
+                <button @click="closeModal()"
+                  class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                </button>
+              </header>
+              <form @submit.prevent="onSubmit">
+                <div class="bg-white px-4 pt-5 pb-4">
 
-                <div class="mt-4">
-                  <button
-                    type="button"
-                    class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                    @click="closeModal"
-                  >
-                    Got it, thanks!
-                  </button>
                 </div>
-              </DialogPanel>
-            </TransitionChild>
-          </div>
+                <footer class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+
+                </footer>
+              </form>
+            </DialogPanel>
+          </TransitionChild>
         </div>
-      </Dialog>
-    </TransitionRoot>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
 <script setup>
-import { computed } from 'vue'
+import { computed, onUpdated, ref } from 'vue'
 import {
-TransitionRoot,
-TransitionChild,
-Dialog,
-DialogPanel,
-DialogTitle,
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
 } from '@headlessui/vue';
+import Loader from '../../components/core/Loader.vue';
 
-const props = defineProps({
-    modelValue: Boolean
+const product = ref({
+  id: props.product.id,
+  title: props.product.title,
+  images: props.product.images,
+  description: props.product.description,
+  price: props.product.price
 })
 
-const emit = defineEmits(['update:modelValue']);
+const loading = ref(false);
+
+const props = defineProps({
+  modelValue: Boolean,
+  product: {
+    required: true,
+    type: Object
+  }
+})
+
+const emit = defineEmits(['update:modelValue', close]);
 
 const show = computed({
-    get: () => props.modelValue,
-    set: (value) => emit('update:modelValue', value)
+  get: () => props.modelValue,
+  set: (value) => emit('update:modelValue', value)
+})
+
+onUpdated(() => {
+  product.value = {
+    id: props.product.id,
+    title: props.product.title,
+    images: props.product.images,
+    description: props.product.description,
+    price: props.product.price
+  }
 })
 
 function closeModal() {
-    show.value = false;
+  show.value = false;
+  emit(close);
 }
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
