6f04df68214034e55d2ecacc5675c28d8bb82335
Ben Jeckson <thanhtikexaw@gmail.com>
2023-05-01 23:47:43 +0630

    fixed product head text

--- admin-spa/src/views/Products/Products.vue
+++ admin-spa/src/views/Products/Products.vue
@@ -1,31 +1,22 @@
 <template>
     <div class="w-full">
-    <div class="flex items-center mb-3">
+        <div class="flex justify-between mb-3">
 
-        <!-- <button @click="showCreateModal" type="submit" class="py-2 px-4 border border-transparent text-sm font-medium
+            <!-- <button @click="showCreateModal" type="submit" class="py-2 px-4 border border-transparent text-sm font-medium
                         rounded-md text-white bg-blue-600 hover:bg-blue-800
                         focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Add New Products
         </button> -->
-
-        <div class="row-auto">
-            <div class="grid grid-cols-2">
-                <div class="w-full">
-                    <h1 class="text-3xl font-semibold">Products</h1>
-                </div>
-                <div class="w-full">
-                    <button @click="showDropModalForm" type="submit" class="py-2 px-4 border border-transparent text-sm font-medium
-                        rounded-md text-white bg-blue-600 hover:bg-blue-800
-                        focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Add New Product
-                    </button>
-                </div>
-            </div>
+            <h1 class="text-3xl font-semibold">Products</h1>
+            <button @click="showDropModalForm" type="submit" class="py-2 px-4 border border-transparent text-sm font-medium
+                    rounded-md text-white bg-blue-600 hover:bg-blue-800
+                    focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Add New Product
+            </button>
         </div>
-    </div>
-    <ProductForm  :class="isDropModelForm ? 'block' : 'hidden'" v-model="isDropModelForm" :product="productModel" />
-    <ProductModal v-model="isShowCreateModal" :product="productModel" />
-    <ProductTable @clickEdit="editProduct" @clickDelete="deleteProcess" />
+        <ProductForm :class="isDropModelForm ? 'block' : 'hidden'" v-model="isDropModelForm" :product="productModel" />
+        <ProductModal v-model="isShowCreateModal" :product="productModel" />
+        <ProductTable @clickEdit="editProduct" @clickDelete="deleteProcess" />
     </div>
 </template>
 <script setup>
@@ -62,23 +53,22 @@ function showCreateModal() {
 }
 
 function showDropModalForm() {
-    productModel.value = {...defaultModel};
+    productModel.value = { ...defaultModel };
     isDropModelForm.value = true;
 }
 
 function deleteProcess() {
-    productModel.value = {...defaultModel};
+    productModel.value = { ...defaultModel };
     isDropModelForm.value = false;
 }
 
 function editProduct(product) {
     store.dispatch('getProduct', product.id)
-            .then(({data}) => {
-                productModel.value = data;
-                isDropModelForm.value = false;
-                isDropModelForm.value = true;
-            })
+        .then(({ data }) => {
+            productModel.value = data;
+            isDropModelForm.value = false;
+            isDropModelForm.value = true;
+        })
 }
 </script>
-<style scoped>
-</style>
+<style scoped></style>
